a9f2d78f1120cce5e7dcff7a2e2beed61bb66546
ashni <ashni@LAPTOP-S2S7M7G8>
2017-06-23 14:38:29 +0530

    startup controller changes

--- WebApplication1/src/WebApplication1/Startup.cs
+++ WebApplication1/src/WebApplication1/Startup.cs
@@ -7,8 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-//using Microsoft.Extensions.Logging;
-
+// Test Changes commit 
 namespace WebApplication1
 {
     public class Startup
